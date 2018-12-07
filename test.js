453a3f72e84ee3ab222bd4c599650d1adfaca223
Blancduman <blancduman@gmail.com>
2018-12-07 23:50:33 +0400

    st

--- test.js
+++ test.js
@@ -1,38 +1,19 @@
 const path = require('path'),
       fs = require('fs'),
       pathType = require('path-type');
-
-  const saverDirs = function(data) {
-    return new Promise((resolve, reject) => {
-      var dirs = '';
-      for (var i = 0; i<data.length; i++) {
-        if (data.includes()){}
-      }
-      // for (var i = 0; i<data.length; i++) {
-      //   fs.lstat(__dirname+'/public/' + data[i], (err, stats) => {
-      //     if (stats.isDirectory())
-      //       dirs.push(data[i]);
-      //   })
-      // }
-      resolve(dirs);
-    });
-  }
-  
-  const saverFiles = function(data) {
-    return new Promise((resolve, reject) => {
-      var files = [];
-      for (var i = 0; i<data.length; i++) {
-        fs.lstat(__dirname+'/public/' + data[i], (err, stats) => {
-          if (stats.isFile())
-          files.push(data[i]);
-        })
-      }
-      resolve(files);
-    });
-  }
-
-var dirs=[], files=[];
+      
+function aaa(data) {
+  return Promise.all(
+    [data.map(function(item) {
+    if (fs.lstatSync(__dirname+'/public/'+item).isDirectory())
+      return item;
+  }), data.map(function(item) {
+    if (fs.lstatSync(__dirname+'/public/'+item).isFile())
+      return item;
+  })]);
+}
 fs.readdir(__dirname+'/public', (err, data) => {
-  dirs = data.map(item => {pathType.dir(__dirname+'/public/'+item).then(_item => {if (_item) console.log (item);})});
-  console.log(dirs, files);
+  aaa(data).then(a => console.log(b =[a[0].filter(el => {
+    return el!=null;}), a[1].filter(el => {return el!=null;})])
+  );
 })
